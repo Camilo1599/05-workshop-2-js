9c111159e2f1248e146192e2f940c705c7d02d7b
Deybison <camimilo2014@gmail.com>
2024-04-16 17:23:45 -0500

    reto 2 terminado

---
+++ seccion#1/reto-2.js
@@ -0,0 +1,39 @@
+const souvenirs = [];
+let flag = false;
+
+const validate = (type, value) => typeof value ===type && !Number.isNaN(value);
+
+
+if (confirm("Desea ingresar un souvenir")){
+    let name = prompt("Ingresa el nombre del producto");
+    let cost = prompt("ingresa el costo del producto");
+    cost = Number(cost);
+    let available = confirm("Esta disponible?");
+    flag = validate('string', name) && validate('number', cost) && validate('boolean',available);
+    if (flag === true){
+        const souvenir = {
+            'name': name,
+            'cost': cost,
+            'available':available
+        }
+        souvenirs.push(souvenir)
+        console.log(souvenirs)
+    }
+}
+
+if (confirm("Desea ingresar un souvenir")){
+    let name = prompt("Ingresa el nombre del producto");
+    let cost = prompt("ingresa el costo del producto");
+    cost = Number(cost);
+    let available = confirm("Esta disponible?");
+    flag = validate('string', name) && validate('number', cost) && validate('boolean',available);
+    if (flag === true){
+        const souvenir = {
+            'name': name,
+            'cost': cost,
+            'available':available
+        }
+        souvenirs.push(souvenir)
+        console.log(souvenirs)
+    }
+}
