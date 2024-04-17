e9965784d9823527ef2151e97a2b1e1b0734d2e2
MahicolGallego <user1@gmail.com>
2024-04-17 15:13:33 -0500

    CAsi final reto 3

--- seccion#1/reto-3.js
+++ seccion#1/reto-3.js
@@ -1,13 +1,42 @@
-const threshold = 200000;
-let finalBudget = initialBudget - dailyBudget;
+const threshold = 200;
+const articles = [];
+let article;
 
-let initialBudget = prompt("Ingresa el presupuesto inicial para este viajer: ");
-let dailyBudget = prompt("Ingresa los gastos diarios en alojamiento, transporte y alimento para este viaje: ");
-let extraArticle = prompt("Ingresa el costo del articulo extra que deseas comprar");
+let nameArticle, extraArticle, finalBudget, betterOption;
 
-if (finalBudget >= threshold){
-    alert("Tu presupuesto es suficiente para comprar el articulo extra")
+let initialBudget = prompt("Ingresa el presupuesto inicial para este viaje: ");
+let dailyBudget = prompt(
+  "Ingresa los gastos diarios en alojamiento, transporte y alimento para este viaje: "
+);
+let days = prompt("Ingresa la cantidad de dias que durara este viaje");
+// nameArticle = prompt("Ingresa el nombre del articulo extra que deseas comprar");
+extraArticle = prompt("Ingresa el costo del articulo extra que deseas comprar");
+finalBudget = initialBudget - dailyBudget * days;
+console.log(extraArticle);
+betterOption = extraArticle;
+console.log(betterOption);
+
+extraArticle = prompt("Ingresa el costo del articulo extra que deseas comprar");
+console.log(extraArticle);
+if (extraArticle < betterOption) {
+  betterOption = extraArticle;
+} else {
+  betterOption = betterOption;
 }
-else{
-    alert("evita gastos adicionales para no sobrepasar el presupuesto")
+console.log(betterOption);
+
+extraArticle = prompt("Ingresa el costo del articulo extra que deseas comprar");
+console.log(extraArticle);
+if (extraArticle < betterOption) {
+  betterOption = extraArticle;
+} else {
+  betterOption = betterOption;
+}
+console.log(betterOption);
+alert("El articulo extra mas economico es el que cuesta $" + betterOption);
+
+if (finalBudget - extraArticle >= threshold) {
+  alert("Tu presupuesto es suficiente para comprar el articulo extra");
+} else {
+  alert("evita gastos adicionales para no sobrepasar el presupuesto");
 }
